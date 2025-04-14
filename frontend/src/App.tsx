4c97191bcc462b31ce524dc07998b10c50843aa4
zippostorm <toghkklli@gmail.com>
2025-04-14 14:36:34 +0300

    shadcn setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div className="text-red-500 text-3xl">Hello</div>;
+  return (
+    <div className="text-red-500 text-3xl">
+      Hello
+      <Button variant={"outline"}>My new button</Button>
+    </div>
+  );
 };
 
 export default App;
